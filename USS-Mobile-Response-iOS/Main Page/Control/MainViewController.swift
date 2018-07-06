bce66836334a722553d6c12df799af177c7c404d
luxondata <luxondata@cyril-imac.local>
2018-07-06 17:19:25 +0800

    Organized files around. Updated Tab Bar icons.

--- USS-Mobile-Response-iOS/Main Page/Control/MainViewController.swift
+++ USS-Mobile-Response-iOS/Main Page/Control/MainViewController.swift
@@ -8,10 +8,9 @@ import Foundation
 import UIKit
 
 class MainViewController: UIViewController {
-    override func viewDidLoad() {
-        super.viewDidLoad()
-    }
+   
 
+    @IBOutlet weak var tabBar: UITabBar!
     let sideMenuLauncher = SideMenuLauncher()
     
     @IBAction func menuTapped(_ sender: Any) {
@@ -19,6 +18,11 @@ class MainViewController: UIViewController {
         
         // Cover and disable main view when hamburger button is tapped.
         sideMenuLauncher.showSideMenu()
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tabBar.selectedItem = tabBar.items?.first
     }
     
 }
