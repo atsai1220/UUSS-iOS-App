1d2b069850dbe4050ff1f0ea324b50bda5be8b87
atsai1220 <14171643+atsai1220@users.noreply.github.com>
2018-07-30 13:37:43 -0600

    Fixed side-menu rotation bug

--- USS-Mobile-Response-iOS/Main Page/Control/MainTabBarController.swift
+++ USS-Mobile-Response-iOS/Main Page/Control/MainTabBarController.swift
@@ -18,13 +18,19 @@ class MainTabBarController: UITabBarController {
         sideMenuLauncher.showSideMenu()
     }
     
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         selectedIndex = 0
         navigationItem.title = "Main"
         navigationController?.navigationBar.prefersLargeTitles = true
     }
-
     
+    override func viewWillLayoutSubviews() {
+        if let window = UIApplication.shared.keyWindow {
+            let cvWidth = 260
+            let oldRect = sideMenuLauncher.collectionView.frame
+            sideMenuLauncher.collectionView.frame = CGRect(x: Int(oldRect.origin.x), y: Int(oldRect.origin.y), width: cvWidth, height: Int(window.frame.height))
+            sideMenuLauncher.greyView.frame = window.frame
+        }
+    }
 }
