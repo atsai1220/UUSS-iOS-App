3708c9a9b74d198e3b3544b5b67f465a5cb9970a
atsai1220 <14171643+atsai1220@users.noreply.github.com>
2018-08-01 21:47:29 -0600

    Fixed rotation bug and overlapping UIViews in UIPageViewController.

--- USS-Mobile-Response-iOS/Form/FormViewController.swift
+++ USS-Mobile-Response-iOS/Form/FormViewController.swift
@@ -49,28 +49,39 @@ class FormViewController: UIPageViewController, UIPageViewControllerDataSource, 
 
         //Create view controllers and view for the view controllers and put them in the viewcontrollerArray
         view1.setColor(newColor: UIColor.blue)
-        page1ViewController.view.addSubview(view1)
         view2.setColor(newColor: UIColor.green)
-        page2ViewController.view.addSubview(view2)
         view3.setColor(newColor: UIColor.purple)
-        page3ViewController.view.addSubview(view3)
+//        
+//        page1ViewController.view.addSubview(view1)
+//        page2ViewController.view.addSubview(view2)
+//        page3ViewController.view.addSubview(view3)
+        
         viewControllersArray.append(page1ViewController)
         viewControllersArray.append(page2ViewController)
         viewControllersArray.append(page3ViewController)
 
         //Set the view controllers for the pagesViewController
         setViewControllers([viewControllersArray[0]], direction: .forward, animated: true, completion: nil)
-        
 
         //Setting the UIPageControl View
         pageControl = UIPageControl(frame: CGRect(x: 0, y: 0, width: UIScreen.main.bounds.maxX, height: UIScreen.main.bounds.maxY))
-        pageControl?.backgroundColor = UIColor.red
+//        pageControl?.backgroundColor = UIColor.red
+        pageControl?.backgroundColor = UIColor(red: 1, green: 0, blue: 0, alpha: 0.25)
         pageControl!.numberOfPages = 3
         pageControl!.currentPage = 0
         pageControl!.pageIndicatorTintColor = UIColor.lightGray
         pageControl!.currentPageIndicatorTintColor = UIColor.darkGray
-        self.view.addSubview(pageControl!)
+//        self.view.addSubview(pageControl!)
         
+        setupViews()
+        
+    }
+    
+    func setupViews() {
+        self.page1ViewController.view.addSubview(view1)
+        self.page2ViewController.view.addSubview(view2)
+        self.page3ViewController.view.addSubview(view3)
+        self.view.addSubview(self.pageControl!)
     }
     
     func pageViewController(_ pageViewController: UIPageViewController, didFinishAnimating finished: Bool, previousViewControllers: [UIViewController], transitionCompleted completed: Bool) {
@@ -109,10 +120,15 @@ class FormViewController: UIPageViewController, UIPageViewControllerDataSource, 
         return nil
     }
     
+    
     override func viewDidLayoutSubviews()
     {
         var rect = CGRect(x: 0.0, y: 0.0, width: UIScreen.main.bounds.maxX, height: UIScreen.main.bounds.maxY)
+        
+        (view1.page!.frame, rect) = rect.divided(atDistance: rect.height * 0.9, from: CGRectEdge.minYEdge)
+        view2.page!.frame = view1.page!.frame
+        view3.page!.frame = view1.page!.frame
 
-        (pageControl!.frame, rect) = rect.divided(atDistance: rect.height * 0.1, from: CGRectEdge.maxYEdge)
+        (pageControl!.frame, rect) = rect.divided(atDistance: rect.height, from: CGRectEdge.minYEdge)
     }
 }
