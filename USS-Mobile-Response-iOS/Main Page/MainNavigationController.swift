30a31d85e1de964e464aae671703bb16dc2e8351
atsai1220 <14171643+atsai1220@users.noreply.github.com>
2018-09-20 22:12:22 -0600

    mid-work on floating button

--- USS-Mobile-Response-iOS/Main Page/MainNavigationController.swift
+++ USS-Mobile-Response-iOS/Main Page/MainNavigationController.swift
@@ -9,9 +9,33 @@ import UIKit
 
 
 class MainNavigationController: UINavigationController {
+    var menuButton = UIImageView(image: UIImage(named: "menu_button"))
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        
+        
+        
+        self.menuButton.layer.shadowColor = UIColor.black.cgColor
+        self.menuButton.layer.shadowOffset = CGSize(width: 5, height: 5)
+        self.menuButton.layer.shadowRadius = 5
+        self.menuButton.layer.shadowOpacity = 0.5
+        self.view.addSubview(self.menuButton)
+    }
     
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+        NSLayoutConstraint.activate([
+            self.menuButton.centerXAnchor.constraint(equalTo: self.view.rightAnchor, constant: -self.menuButton.layer.frame.size.width),
+            self.menuButton.bottomAnchor.constraint(equalTo: self.view.bottomAnchor, constant: -self.menuButton.layer.frame.size.height * 1.25),
+            self.menuButton.widthAnchor.constraint(equalToConstant: 60),
+            self.menuButton.heightAnchor.constraint(equalToConstant: 60)
+            ])
+        self.menuButton.layer.cornerRadius = self.menuButton.layer.frame.size.width / 2
+        self.menuButton.backgroundColor = UIColor.white
+        self.menuButton.layer.masksToBounds = false
+        self.menuButton.translatesAutoresizingMaskIntoConstraints = false
     }
 
 }
